a091df5cdfc1802230d768e9420ab43fba33eee8
Jorge Mario Gutierrez Ramirez <jorgem.gutierrezr@gmail.com>
2026-01-03 22:22:29 -0500

    Mi primer commit

---
+++ sender_stand_request.py
@@ -0,0 +1,38 @@
+import configuration
+import requests
+import data
+
+def get_logs():
+    return requests.get(configuration.URL_SERVICE + configuration.LOG_MAIN_PATH, params={"count": 20})
+
+response = get_logs()
+print(response.status_code)
+print(response.headers)
+
+def get_users_table():
+    return requests.get(configuration.URL_SERVICE + configuration.USERS_TABLE_PATH)
+
+response = get_users_table()
+print(response.status_code)
+
+CREATE_USER_PATH = "/api/v1/users/"
+
+def post_new_user(body):
+    return requests.post(configuration.URL_SERVICE + configuration.CREATE_USER_PATH,  # inserta la dirección URL completa
+                         json=body,  # inserta el cuerpo de solicitud
+                         headers=data.headers)  # inserta los encabezados
+
+response = post_new_user(data.user_body)
+
+print(response.status_code)
+
+print(response.json())
+
+def post_products_kits(products_ids):
+    return requests.post(configuration.URL_SERVICE + configuration.PRODUCTS_KITS_PATH, json=products_ids, headers=data.headers)
+
+response = post_products_kits(data.product_ids)
+print(response.status_code)
+print(response.json())
+
+
